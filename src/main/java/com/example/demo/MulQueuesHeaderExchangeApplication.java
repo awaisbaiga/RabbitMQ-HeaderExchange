0f5732604ea41f8c71fe01f17b4a127f63d6075f
Awais Baiga <70507684+awaisbaiga@users.noreply.github.com>
2020-09-08 12:27:36 +0500

    Create HeaderExchange For MulQueues

---
+++ src/main/java/com/example/demo/MulQueuesHeaderExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulQueuesHeaderExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulQueuesHeaderExchangeApplication.class, args);
+	}
+
+}
